959ac1a7556e94232d13ea7f17dc111a4918c857
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2024-10-23 22:30:19 +0300

    [refactor] Utils weighted_choice

--- project/utils/utils.py
+++ project/utils/utils.py
@@ -8,12 +8,12 @@ from project.wfc.wobj import WeightedObject
 class Utils:
     @staticmethod
     def weighted_choice(
-        possible_objects: List[WeightedObject], seed: int = None
+        objects: List[WeightedObject], seed: int = None
     ) -> WeightedObject:
         """Select a weighted object based on its weight."""
         random_gen = np.random.RandomState(seed)
 
-        weights = np.array([obj.weight for obj in possible_objects])
+        weights = np.array([obj.weight for obj in objects])
         probabilities = weights / np.sum(weights)
 
-        return random_gen.choice(possible_objects, p=probabilities)
+        return random_gen.choice(objects, p=probabilities)
