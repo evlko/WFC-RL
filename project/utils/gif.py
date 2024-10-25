599b6493b324e7f4be313cf8a4c63ed255fc8e35
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2024-10-25 12:52:29 +0300

    [add] gif util

---
+++ project/utils/gif.py
@@ -0,0 +1,15 @@
+import glob
+
+from PIL import Image
+
+
+def create_gif(output_path: str, image_folder: str, duration: int = 100):
+    """Create a GIF from PNG images."""
+    images = []
+    for filename in sorted(glob.glob(f"{image_folder}/*.png")):
+        img = Image.open(filename)
+        images.append(img)
+
+    images[0].save(
+        output_path, save_all=True, append_images=images[1:], duration=duration, loop=0
+    )
