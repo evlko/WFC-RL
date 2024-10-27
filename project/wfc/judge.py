6cc3d300c7ed7543a904d54d5bc325d0731197b3
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2024-10-27 17:27:01 +0300

    [add] grid param to judge select

--- project/wfc/judge.py
+++ project/wfc/judge.py
@@ -2,23 +2,30 @@ from abc import ABC, abstractmethod
 from typing import List
 
 from project.utils.utils import Utils
+from project.wfc.grid import Grid
 from project.wfc.wobj import WeightedObject
 
 
 class Judge(ABC):
     @staticmethod
     @abstractmethod
-    def select(objects: List[WeightedObject], seed: int = None) -> WeightedObject:
+    def select(
+        objects: List[WeightedObject], grid: Grid | None = None, seed: int = None
+    ) -> WeightedObject:
         pass
 
 
 class RandomJudge(Judge):
     @staticmethod
-    def select(objects: List[WeightedObject], seed: int = None) -> WeightedObject:
+    def select(
+        objects: List[WeightedObject], grid: Grid | None = None, seed: int = None
+    ) -> WeightedObject:
         return Utils.weighted_choice(objects=objects, seed=seed)
 
 
 class GreedyJudge(Judge):
     @staticmethod
-    def select(objects: List[WeightedObject], seed: int = None) -> WeightedObject:
+    def select(
+        objects: List[WeightedObject], grid: Grid | None = None, seed: int = None
+    ) -> WeightedObject:
         return max(objects, key=lambda obj: obj.weight)
