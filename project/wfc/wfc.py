173ade4edae16163838ed749c201b864be0468b7
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2024-10-27 17:27:25 +0300

    [refactor] wfc step to reduce potential bugs

--- project/wfc/wfc.py
+++ project/wfc/wfc.py
@@ -16,12 +16,12 @@ class StepResult:
 
 
 class WFC:
-    def __init__(self, grid: Grid, judge: Judge):
+    def __init__(self, grid: Grid, judge: Judge) -> None:
         self.grid = grid
         self.judge = judge
         self._is_initialized = False
 
-    def initialize(self):
+    def _initialize(self) -> None:
         """Initialize the grid for the WFC process."""
         self.grid.initialize()
         self._is_initialized = True
@@ -30,7 +30,7 @@ class WFC:
         """Perform one step in the WFC process: find, collapse, and update neighbors."""
         result = StepResult()
         if not self._is_initialized:
-            self.initialize()
+            self._initialize()
 
         # find point and fail if None
         point = self.grid.find_least_entropy_cell()
@@ -40,18 +40,18 @@ class WFC:
             return result
 
         # find possible patterns and fail if None
-        possible_patterns = self.grid.get_valid_patterns(point)
+        possible_patterns = self.grid.get_valid_patterns(p=point)
         if not possible_patterns and early_stopping:
             result.failed_point = point
             return result
 
         # get random pattern from judge and place it
-        chosen_pattern = self.judge.select(possible_patterns)
+        chosen_pattern = self.judge.select(objects=possible_patterns, grid=self.grid)
         result.chosen_pattern = chosen_pattern
-        self.grid.place_pattern(point, chosen_pattern)
+        self.grid.place_pattern(p=point, pattern=chosen_pattern)
 
         # find a cell with zero entropy and fail if one such exists
-        zero_entropy_cell = self.grid.update_neighbors_entropy(point)
+        zero_entropy_cell = self.grid.update_neighbors_entropy(p=point)
         if zero_entropy_cell and early_stopping:
             result.failed_point = zero_entropy_cell
             return result
@@ -62,7 +62,7 @@ class WFC:
 
     def generate(self) -> bool:
         """Run the generation process until the grid is fully collapsed or fails."""
-        self.initialize()
+        self._initialize()
         while not self.is_complete():
             if not self.step().success:
                 return False
